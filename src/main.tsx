f40eb228c62d290a6c445d70a210f62fea3e0617
Candra Sidik Dermawan <86487468+candrasdkd@users.noreply.github.com>
2026-02-20 08:36:50 +0700

    feat: Implement and configure PWA Service Worker registration with development options.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
